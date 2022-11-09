45edaf38607ca2641eeba9eb27c8b63569d27321
amJayem <csejayem19@gmail.com>
2022-11-09 11:26:36 +0600

    services are getting from db

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
 
@@ -9,6 +10,29 @@ const port = process.env.PORT || 5000;
 // middle wares
 app.use(cors())
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.42e2srw.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run (){
+    try{
+        const serviceCollection = client.db('dental-care').collection('services');
+
+        // reading data from database
+        app.get('/services', async(req,res)=>{
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+
+            res.send(services);
+        });
+    }
+    finally{}
+}
+
+run().catch(e=>console.log(e));
 
 app.get('/', (req, res)=>{
     res.send('Dental care server is running...');
